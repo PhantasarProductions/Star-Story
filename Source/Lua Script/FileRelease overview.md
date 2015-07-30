747dc59fb614d179b28fff76e1b9336389b86c0b
Jeroen Broks <facebook@tbbs.nl>
2015-07-30 19:45:16 +0200

    Script update 15.07.30

--- Source/Lua Script/FileRelease overview.md
+++ Source/Lua Script/FileRelease overview.md
@@ -101,4 +101,4 @@
 
 
 
-### Last updated: Thu 30 July 2015; 19:43:52 CET
+### Last updated: Thu 30 July 2015; 19:45:16 CET
--- Source/Lua Script/Script/Use/Combat/CDrawFighters.lua
+++ Source/Lua Script/Script/Use/Combat/CDrawFighters.lua
@@ -89,7 +89,7 @@ if not ReportList then return end
 for fg,fgl in spairs(ReportList) do for fi,f in pairs(fgl) do
     if #f>0 then
        --Image.Scale(f[1].scale,f[1].scale)
-       Image.Font("Robotica.ttf",round(f[1].scale))
+       Image.Font("Fonts/Robotica.ttf",round(f[1].scale))
        DarkText(f[1].m,f[1].x,f[1].y,2,2,f[1].r,f[1].g,f[1].b)
        if f[1].scale<15 then f[1].scale = f[1].scale + .5
        elseif f[1].timer>0 then f[1].timer = f[1].timer -1 
