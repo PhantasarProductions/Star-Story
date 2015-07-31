9749bfb36328af873873a146c8c28dda63e84bcb
Jeroen Broks <facebook@tbbs.nl>
2015-07-31 15:27:09 +0200

    Script update 15.07.31

--- Source/Lua Script/FileRelease overview.md
+++ Source/Lua Script/FileRelease overview.md
@@ -102,4 +102,4 @@
 
 
 
-### Last updated: Fri 31 July 2015; 15:26:09 CET
+### Last updated: Fri 31 July 2015; 15:27:09 CET
--- Source/Lua Script/Script/Use/Combat/CDrawFighters.lua
+++ Source/Lua Script/Script/Use/Combat/CDrawFighters.lua
@@ -166,7 +166,7 @@ myfoe.DeathScale = myfoe.DeathScale or 100
 Image.ScalePC(myfoe.DeathScale,100)   
 Image.Show(pt[Neg[idx]==true]..Fighters.Foe[idx].Tag,CoordsFighter.Foe(idx)) -- Neg[idx]==true is used, as the initial value is "nil" and that would cause the game to crash. CoordsFigher.Foe(idx) will return both the x and the y, and the Lua parser will pick that up correctly.
 if RPGStat.Points(myfoe.Tag,"HP").Have==0 then
-   myfoe.DeathScale = myfoe.Deathscale - 1
+   myfoe.DeathScale = myfoe.DeathScale - 1
    if myfoe.DeathScale<=0 then
        KillFoe(idx,myfoe.Tag) 
        end
