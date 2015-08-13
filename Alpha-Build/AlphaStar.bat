3b7272696b39c433d91ca63e009d5c76da6c5c57
Jeroen Broks <facebook@tbbs.nl>
2015-08-13 17:11:23 +0200

    Preparation Alpha
    Set up the windows installer/updater/runner

---
+++ Alpha-Build/AlphaStar.bat
@@ -0,0 +1,40 @@
+@echo off
+echo off
+rem The second "echo off" command is to make sure the echo is off, as the "@" prefix is should be supported in all official versions of Windows, but if you use something like a "windows clone" I cannot guarantee that :)
+
+if exist "C:\program files\Git\bin\git.exe" goto gitfound
+echo "* ERROR *"
+echo " Git could not be found, or at least not in the place where I expected it"
+goto bye
+
+
+:gitfound
+if exist "Installed.txt" goto pull
+echo: - Installing Star Story Alpha
+echo:   = Configuring git
+md AlphaBuild
+C:\"program files"\git\bin\git init AlphaBuild
+cd AlphaBuild
+git remote add -f origin https://github.com/Tricky1975/Star-Story.git
+C:\"program files"\git\bin\git config core.sparseCheckout true
+echo WindowsExe >> .git/info/sparse-checkout
+echo JCR >> .git/info/sparse-checkout
+echo:   = Downloading game
+C:\"Program Files"\git\bin\git pull origin master
+goto run
+
+:pull
+echo: - Updating
+cd AlphaBuild
+C:\"Program Files"\git\bin\git pull origin mastr
+goto run
+
+:run
+echo: - Running the game
+WindowsExe\"Star Story"
+
+
+
+
+:bye
+echo: - Batch job ended
