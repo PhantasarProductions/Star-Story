09e893851b2d9f0a00888db7ea18bd2a458730d8
Jeroen Broks <facebook@tbbs.nl>
2015-08-16 20:27:51 +0200

    Windows update 15.08.16

---
+++ UpdateGit.sh
@@ -0,0 +1,3 @@
+git add --all
+git commit -m "Windows update 15.08.16"
+git push
---
+++ WinUpdateGit.bat
@@ -0,0 +1,3 @@
+C:\"Program files"\git\bin\git add --all
+C:\"Program files"\git\bin\git commit -m "Windows update 15.08.16"
+C:\"Program files"\git\bin\git push
