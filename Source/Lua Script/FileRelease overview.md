e028c29327bb0f121285d85e85d14834f1c3ab3d
Jeroen Broks <facebook@tbbs.nl>
2015-08-16 22:24:04 +0200

    Script update 15.08.16

--- Source/Lua Script/FileRelease overview.md
+++ Source/Lua Script/FileRelease overview.md
@@ -103,4 +103,4 @@
 
 
 
-### Last updated: Sun 16 August 2015; 22:21:35 CET
+### Last updated: Sun 16 August 2015; 22:24:03 CET
--- Source/Lua Script/Script/Flow/Field.lua
+++ Source/Lua Script/Script/Flow/Field.lua
@@ -206,8 +206,8 @@ local myleveltotal,partymembers,ptag
 -- Make sure there are no foes in the field
 CSay("Searching for existing foes")
 for obj in KthuraEach("Actor") do
-    CSay("Checking: "..obj.Kind.." "..obj.Tag.."; Got suffix "..sval(suffixed(obj.Tag," FoeActor")))
-    if suffixed(obj.Tag," FoeActor") then
+    CSay("Checking: "..obj.Kind.." "..obj.Tag.."; Got suffix "..sval(suffixed(obj.Tag,"FoeActor")))
+    if suffixed(obj.Tag,"FoeActor") then
        Maps.Kill(obj.Tag); CSay("Killed foe: "..obj.Tag.." (Obj #"..obj.IDNum..")")
        end 
     end
