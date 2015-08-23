1aa9006e5b04e838e7eb4080106920cf62d2ea20
Jeroen Broks <facebook@tbbs.nl>
2015-08-23 23:19:31 +0200

    Mac update 15.08.23

--- Source/Lua Script/Script/Use/Linkers/FieldLinker.lua
+++
@@ -1,8 +0,0 @@
-LoadMap = LoadMap or function(map) -- This way of defining makes sure we won't (by accident) overwrite the 'real' routine if it's present.
-MS.LN_Run("FIELD","Flow/Field.lua","LoadMap",map)
-end
-
-
-Schedule = Schedule or function (scr,func)
-MS.LN_Run("FIELD","Flow/Field.lua","Schedule",scr..";"..func) 
-end
