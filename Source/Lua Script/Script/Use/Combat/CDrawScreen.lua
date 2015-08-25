cfac76bd98321dba29b7b29cda87723578a38f84
Jeroen Broks <facebook@tbbs.nl>
2015-08-25 15:24:08 +0200

    Source Update 15.08.25.7

--- Source/Lua Script/Script/Use/Combat/CDrawScreen.lua
+++ Source/Lua Script/Script/Use/Combat/CDrawScreen.lua
@@ -101,7 +101,7 @@ SetFont("CombatMessage")
 for m in each ( CombatMessages ) do
     Image.SetAlphaPC(m.Alpha)
     if m.Icon then
-       DrawImage(m.Icon,m.IconX,m.y)
+       Image.Show(m.Icon,m.IconX,m.y)
        end
     DarkText(m.Msg,400,m.y,2,2,0,180,255)    
     if m.SPD>0 then
