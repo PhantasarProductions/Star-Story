78d3188ea9300f36f2fd60e8d25d5e71c38d5fe1
Jeroen Broks <facebook@tbbs.nl>
2015-08-25 21:20:37 +0200

    Source Update 15.08.25.35

--- Source/Lua Script/FileRelease overview.md
+++ Source/Lua Script/FileRelease overview.md
@@ -110,4 +110,4 @@
 
 
 
-### Last updated: Tue 25 August 2015; 21:17:50 CET
+### Last updated: Tue 25 August 2015; 21:20:35 CET
--- Source/Lua Script/Script/Flow/GameOver.lua
+++ Source/Lua Script/Script/Flow/GameOver.lua
@@ -9,16 +9,15 @@ end
 
 function MAIN_FLOW()
 White()
-if CombatAlpha>0 then CombatAlpha = CombatAlpha - .01 end
 Image.Cls()
 Image.SetAlphaPC(100)
 Image.Show(GameOver,400,300)
 Image.SetAlphaPC(CombatAlpha)
 Image.Show(Combat,0,0)
 Image.SetAlphaPC(100)
-if CombatAlpha>0 then CombatAlpha = CombatAlpha - .01 else 
+if CombatAlpha>0 then CombatAlpha = CombatAlpha - .05 else 
    SetFont("CombatMessage")
-   DarkText("Hit any key or press a mousebutton to exit",400,550,2,2,0,math.sin(Time.MSecs()/100)*180,255) 
+   DarkText("Hit any key ",400,550,2,2,0,math.sin(Time.MSecs()/200)*180,255) 
    end
 Flip()   
 local dev,k
