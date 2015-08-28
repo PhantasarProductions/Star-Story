7a4d8d25d3730b19cb90459ae48dd759beb5d2a0
Jeroen Broks <facebook@tbbs.nl>
2015-08-29 00:23:52 +0200

    Mac update 15.08.29

--- Source/Lua Script/FileRelease overview.md
+++ Source/Lua Script/FileRelease overview.md
@@ -114,4 +114,4 @@
 
 
 
-### Last updated: Sat 29 August 2015; 00:20:52 CET
+### Last updated: Sat 29 August 2015; 00:23:05 CET
--- Source/Lua Script/Script/Flow/Field.lua
+++ Source/Lua Script/Script/Flow/Field.lua
@@ -539,6 +539,7 @@ for k,t in spairs(FieldTreasure) do
           MS.LoadNew("BOXTEXT","Script/SubRoutines/BoxText.lua")
           MS.Run("BOXTEXT","LoadData","TUTORIAL/BAGSFULL;BAGSFULL")
           SerialBoxText("BAGSFULL",upper("FULL."..activecharacter),"Field")
+          SerialBoxText("BAGSFULL","TUTORIAL_FULL","Field")
           MS.Run("BOXTEXT","RemoveData","BAGSFULL")
           Maps.Obj.Kill(t.objtag)
           FieldTreasure[k]=nil
