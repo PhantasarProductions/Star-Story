182b780ba8bb07af55a765030a206ecd81caf94d
Jeroen Broks <facebook@tbbs.nl>
2015-09-15 03:02:02 +0200

    Source Update 15.09.15.2

--- Source/Lua Script/Script/Flow/Field.lua
+++ Source/Lua Script/Script/Flow/Field.lua
@@ -1,6 +1,6 @@
 --[[
   Field.lua
-  Version: 15.09.12
+  Version: 15.09.15
   Copyright (C) 2015 Jeroen Petrus Broks
   
   ===========================
@@ -348,7 +348,7 @@ for obj in KthuraEach() do
           -- new
           repeat
           num = rand(1,9)
-          until rand(1,num)<=skill
+          until num<2 or rand(1,num*((4-skill)*(4-skill)))<=skill
           hilevel = 0
           for ak=1,num do
               foe.Enemies[ak] = 
