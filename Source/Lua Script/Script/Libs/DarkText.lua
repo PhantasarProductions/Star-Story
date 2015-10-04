439f561852c160dac7e0e90cd2c27d6ab46a0e57
Jeroen Broks <facebook@tbbs.nl>
2015-10-04 20:26:02 +0200

    Source Update 15.10.04.3 DOH!

--- Source/Lua Script/Script/Libs/DarkText.lua
+++ Source/Lua Script/Script/Libs/DarkText.lua
@@ -1,17 +1,14 @@
 --[[
-/* 
-  DarkText
-
+  DarkText.lua
+  
+  version: 15.10.04
   Copyright (C) 2015 Jeroen P. Broks
-
   This software is provided 'as-is', without any express or implied
   warranty.  In no event will the authors be held liable for any damages
   arising from the use of this software.
-
   Permission is granted to anyone to use this software for any purpose,
   including commercial applications, and to alter it and redistribute it
   freely, subject to the following restrictions:
-
   1. The origin of this software must not be misrepresented; you must not
      claim that you wrote the original software. If you use this software
      in a product, an acknowledgment in the product documentation would be
@@ -19,14 +16,10 @@
   2. Altered source versions must be plainly marked as such, and must not be
      misrepresented as being the original software.
   3. This notice may not be removed or altered from any source distribution.
-
-*/
-
-
-
-Version: 15.07.18
-
 ]]
+
+DTSW = DTSW or 795 -- Should be changed by the script if any other screenwidth than 800 is used. 
+
 function DarkText(txt,x,y,ah,av,r,g,b,br,bg,bb)
 Image.Color(br or 0,bg or 0,bb or 0)
 local ax,ay
@@ -40,6 +33,6 @@ function FitText(txt,x,y,r,g,b)
 local w = Image.TextWidth(txt)
 local ha = 0
 local tx = x
-if x+w>795 then tx=795 ha=2 end
+if x+w>DTSW then tx=DTSW ha=1 end
 DarkText(txt,tx,y,ha,0,r,g,b)
 end
