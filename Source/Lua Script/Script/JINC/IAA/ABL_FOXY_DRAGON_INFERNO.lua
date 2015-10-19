74b47399c9f0e4d1f4c8151ebec4d05b2194a859
Jeroen Broks <facebook@tbbs.nl>
2015-10-19 19:58:08 +0200

    Source Update 15.10.19.9 Added Dragon Burn and Dragon Inferno. Foxy will get those spells but cannot yet learn them.

---
+++ Source/Lua Script/Script/JINC/IAA/ABL_FOXY_DRAGON_INFERNO.lua
@@ -0,0 +1,24 @@
+ret = {
+	["ABL_AP"] = 100,
+	["ABL_APCut"] = 2000,
+	["ABL_DblPower"] = 3000,
+	["ABL_DblSpeed"] = 10000,
+	["ABL_Pose"] = "Cast",
+	["ActSpeed"] = 100,
+	["AttackElement"] = "Fire",
+	["AttackPower"] = 175,
+	["AttackStat"] = "Will",
+	["DefenseStat"] = "Resistance",
+	["Description"] = "Make Reggie set all enemies on fire",
+	["HealingType"] = "Absolute",
+	["Icon"] = "GFX/Elements/Fire.png",
+	["ItemType"] = "Consumable",
+	["Name"] = "Dragon Inferno",
+	["Target"] = "AF",
+	["UseCombat"] = true,
+	["UseField"] = true}
+
+return ret
+
+-- This file is an automatically generated file!
+
