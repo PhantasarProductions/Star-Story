2263bad299520a1cbb7621d8e1644097c161ff9f
Jeroen Broks <facebook@tbbs.nl>
2015-10-19 21:42:01 +0200

    Source Update 15.10.19.14 And a few more things fixed as this didnt work the way it should

--- Source/Lua Script/Script/Use/Combat/XChar.lua
+++ Source/Lua Script/Script/Use/Combat/XChar.lua
@@ -126,9 +126,9 @@ XCharAfterAction = {
 				local r
 				CSay("Let's see if Foxy learns something new!")
 				for abl,rate in pairs(abilities) do
-					hasit = RPGChar.ListHas("Foxy","ABL",abl)
+					hasit = RPGChar.ListHas("Foxy","ABL",abl)==1
 					hasall = hasall and hasit
-					learnit = RPGChar.ListHas("Foxy","LEARN",abl)
+					learnit = RPGChar.ListHas("Foxy","LEARN",abl)==1
 					allowlearn = not(hasit or learnit)
 					r = rand(1,rate)
 					CSay("Ability: "..abl.."; hasit: "..sval(hasit).."; hasall: "..sval(hasall).."; learnit: "..sval(learnit).."; allowlearn: "..sval(allowlearn).."; rate: "..rate.."; random: "..r)
