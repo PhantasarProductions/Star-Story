799f911b31f98b81da56b89e87fd31fa24a16ef0
Jeroen Broks <facebook@tbbs.nl>
2016-08-12 10:40:37 +0200

    Removed unneeded shit

--- UpdateGit.sh
+++
@@ -1,3 +0,0 @@
-git add --all
-git commit -m "Windows update 16.08.12"
-git push
--- WinUpdateGit.bat
+++
@@ -1,3 +0,0 @@
-C:\"Program files"\git\bin\git add --all
-C:\"Program files"\git\bin\git commit -m "Windows update 16.08.12"
-C:\"Program files"\git\bin\git push
